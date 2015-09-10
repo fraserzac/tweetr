0ec02c6dc3e742583fbee0d6eeb51df3a67cc2e5
Zachary Fraser <zacharyfraser@Zacharys-MacBook-Pro.local>
2015-09-10 16:20:00 +1000

    again

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -20,6 +20,7 @@ class SessionController < ApplicationController
 
 	def destroy
 		session[:user_id] = nil
+		@current_user = nil
 		redirect_to root_path
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   get '/users/unfollow/:id' => 'users#unfollow', as: 'unfollow'
   resources :users, :pages, :posts
 
-  get '/login' => 'session#new'
+  get '/login' => 'session#new', as: 'login'
   post '/login' => 'session#create'
-  delete '/login' => 'session#destroy'
+  delete '/login' => 'session#destroy', as: 'logout'
   get '/post' => 'session#create'
   get '/account' => 'pages#create'
   get '/edit/:id' => 'posts#edit', :as => 'post_edit'
